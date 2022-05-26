868789dbce5f63a12c48429fd350547aeee3c5dd
nexus4812 <powerbatteryboy@gmail.com>
2022-05-26 11:56:03 +0900

    Refact nvm install with homebrew

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,3 +3,8 @@ readfile() {[ -r $1 ] && . $1 }
 readfile "/usr/local/etc/profile.d/bash_completion.sh"
 readfile "$HOME/.git-prompt.sh"
 readfile "$HOME/.nvm/nvm.sh"
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+readfile "/usr/local/opt/nvm/nvm.sh" # This loads nvm
+readfile "/usr/local/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
