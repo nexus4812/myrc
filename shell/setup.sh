3dc250f9dac66a388fe0302d6256c6399c326026
nexus4812 <powerbatteryboy@gmail.com>
2022-05-26 15:48:19 +0900

    Add golang path

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,5 +1,11 @@
 readfile() {[ -r $1 ] && . $1 }
 
+# Golang path
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
+
+
 readfile "/usr/local/etc/profile.d/bash_completion.sh"
 readfile "$HOME/.git-prompt.sh"
 readfile "$HOME/.nvm/nvm.sh"
