cd60aed72d360715c39c54725c308dad69063892
nexus4812 <powerbatteryboy@gmail.com>
2022-05-27 11:10:28 +0900

    Refact

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,16 +1,16 @@
 readfile() {[ -r $1 ] && . $1 }
 
-# Golang path
-export GOPATH=$HOME/go
-export GOBIN=$GOPATH/bin
-export PATH=$PATH:$GOBIN
-
-
 readfile "/usr/local/etc/profile.d/bash_completion.sh"
 readfile "$HOME/.git-prompt.sh"
-readfile "$HOME/.nvm/nvm.sh"
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
 readfile "/usr/local/opt/nvm/nvm.sh" # This loads nvm
 readfile "/usr/local/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+
+# Golang path
+if type "go" > /dev/null 2>&1; then
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
+fi
